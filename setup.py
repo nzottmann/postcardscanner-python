09f44761ca53ee102d1f1cb2f1348f689a0ced77
Nils Zottmann <n.zottmann@mozys.de>
2022-10-07 19:03:57 +0200

    1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="postcardscanner-nzottmann",
-    version="1.0.0",
+    version="1.1.0",
     author="Nils Zottmann",
     author_email="mail@nils-zottmann.de",
     description="Library for postcard scanner",
